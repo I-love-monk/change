d4fa39fa71ab842a24e88c2b598097cec59db078
zhuhuarui <zhuhuaruicn@163.com>
2017-10-13 00:01:05 +0800

    1，补充拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,20 @@ import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
 
+// 拦截器
+Vue.http.interceptors.push(function (req, next) {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('拦截req:', req);
+  }
+  // 拦截res
+  next(function (res) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('拦截res', res);
+      this.$message.error(res);
+    }
+  });
+});
+
 // Vue.http.options.xhr = {withCredentials: true}; // 携带cookie
 Vue.http.options.credentials = true;  // 携带鉴权
 Vue.http.options.emulateJSON = true;  // form传输
